ba6106fd45ea192aaf4128dcbf35247cb5ae284e
Pedro <contato.nevespedro@gmail.com>
2023-02-07 15:32:22 -0300

    adicionado constantes de url

--- src/main/java/packages/Main.java
+++ src/main/java/packages/Main.java
@@ -1,27 +1,36 @@
 package packages;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.Map;
+
 
 public class Main {
 
-    private static final String API_PATH = "/sport/football/livescores";
+    private static final String API_PATH = "/sport/football/team/search";
     private static final String PERSONAL_API_KEY = System.getenv("PERSONAL_API_KEY");
-    private static final String POST_API_URL = "http://api.isportsapi.com/"+API_PATH+"?api_key="+PERSONAL_API_KEY;
+    private static final String TEAM_NAME = "&name=Ibis%20SC";
+    private static final String TEAM_ID = "&teamID=30699";
+    private static final String SEARCH_FOR_ID_URL = "http://api.isportsapi.com/"+API_PATH+"?api_key="+PERSONAL_API_KEY+TEAM_ID;
+    private static final String SEARCH_FOR_NAME_URL = "http://api.isportsapi.com/"+API_PATH+"?api_key="+PERSONAL_API_KEY+TEAM_NAME;
     public static void main(String[] args) throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .GET()
                 .header("accept","application/json")
-                .uri(URI.create(POST_API_URL))
+                .uri(URI.create(SEARCH_FOR_NAME_URL))
                 .build();
 
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
         System.out.println(response.body());
+
+        // parsing json into objects
+
+        ObjectMapper mapper = new ObjectMapper();
     }
 }
