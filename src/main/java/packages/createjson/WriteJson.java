acee38fe096b8661ca8da11769f3df5a35cc0a77
Pedro <contato.nevespedro@gmail.com>
2023-02-13 00:26:25 -0300

    criado metodo para escrever json em lista

--- src/main/java/packages/createjson/WriteJson.java
+++ src/main/java/packages/createjson/WriteJson.java
@@ -1,11 +1,14 @@
 package packages.createjson;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import packages.model.TeamData;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WriteJson {
     private JSONObject jsonObject;
@@ -16,6 +19,26 @@ public class WriteJson {
         aux.putOpt("logo",team.getLogo());
 
         this.jsonObject = aux;
+        createFile();
+    }
+
+    public WriteJson(List<TeamData> teams) throws JSONException {
+        List<JSONObject> list = new ArrayList<>();
+        JSONObject obj = new JSONObject();
+        JSONArray arr = new JSONArray();
+
+        for (int i = 0; i < teams.size(); i++) {
+            JSONObject aux = new JSONObject();
+            aux.put("name", teams.get(i).getName());
+            aux.put("logo", teams.get(i).getLogo());
+
+            list.add(aux);
+        }
+        for (int i = 0; i < list.size(); i++) {
+            arr.put(i, list.get(i));
+        }
+        obj.put("teams", arr);
+        this.jsonObject = obj;
         createFile();
     }
 
